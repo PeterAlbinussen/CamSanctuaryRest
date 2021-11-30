b2e0e2776f359f2ee10b75e3f31fc0f08abf00ca
benjahauge <Benjahauge@hotmail.com>
2021-11-30 10:13:09 +0100

    SenderManager GetAll Tests

--- CamSanctuaryRest/Managers/SenderManager.cs
+++ CamSanctuaryRest/Managers/SenderManager.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using CamSanctuaryRest.Models;
 
 namespace CamSanctuaryRest.Managers
 {
     public class SenderManager
     {
-       
+
+	    private readonly CamsanctuarydbContext _context;
+        public SenderManager(CamsanctuarydbContext context)
+        {
+	        _context = context;
+        }
+
         public SenderManager()
         {
+	        
+        }
 
+        public IEnumerable<Message> GetAll()
+        {
+	        return _context.Messages.ToList();
         }
     }
 }
---
+++ CamSanctuaryRestTests/Managers/SenderManagerTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CamSanctuaryRest.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CamSanctuaryRest.Models;
+
+namespace CamSanctuaryRest.Managers.Tests
+{
+	[TestClass()]
+	public class SenderManagerTests
+	{
+		private static CamsanctuarydbContext context = new CamsanctuarydbContext();
+		private SenderManager manager;
+
+		public SenderManagerTests()
+		{
+			manager = new SenderManager(context);
+		}
+
+		[TestMethod()]
+		public void SenderGetAllTest()
+		{
+			int expectedResult = 0;
+			Assert.AreEqual(expectedResult, manager.GetAll().Count());
+		}
+	}
+}
